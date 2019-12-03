a8628efe8e48207a9def311b1594905c5aecc750
arturgawlik <artur.gawlik@icloud.com>
2019-12-03 22:50:38 +0100

    dealing with nestjs and passport auth

---
+++ server/work-time-notebook/src/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersModule } from 'src/users/users.module';
+import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [UsersModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),],
+  exports: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy]
+})
+export class AuthModule { }
